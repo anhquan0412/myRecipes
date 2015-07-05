0a348e8c298bcf9032c43277bbb46527a5996516
Quan Tran <anhquan0412@gmail.com>
2015-07-05 07:39:47 +0000

    build many-to-many association style, ingredient and recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,13 +2,19 @@ class Recipe < ActiveRecord::Base
   belongs_to :chef
   has_many :likes
   
+  has_many :styles, through: :recipe_styles
+  has_many :recipe_styles
+  
+  has_many :ingredients, through: :recipe_ingredients
+  has_many :recipe_ingredients
+  
   
   validates :name, presence: true,
                     length: {minimum: 5, maximum: 60}
   validates :summary, presence: true, 
                     length: {minimum: 5, maximum: 150}
   validates :description, presence: true, 
-                    length: {minimum: 20, maximum: 500}
+                    length: {minimum: 20, maximum: 5000}
   validates :chef_id, presence: true
   
   mount_uploader :picture, PictureUploader #PictureUploader is name of the class in picture_uploader.rb
---
+++ app/models/style.rb
@@ -0,0 +1,9 @@
+class Style < ActiveRecord::Base
+  has_many :recipes, through: :recipe_styles
+  has_many :recipe_styles
+  
+  
+  validates :name, presence: true, length: {minimum: 1, maximum: 25}
+  
+  
+end
